5a940f1e497b371fbe4d8e266d74d3b4e17ad64f
Alexandre Pereira <programming.hubmaking@slmail.me>
2022-07-05 15:16:31 +0100

    renamed files, added build script
    renamed files, adding underscore to be clear they aren't routines
    
    add script to update the nightly.txt routine

---
+++ dragon-cluster/build_nightly.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env sh
+
+FILE="/tmp/nightly.new"
+
+if (test -e $FILE); then
+    echo "cleaning $FILE"
+    rm $FILE
+fi
+echo "## KF5" >> $FILE
+cat _kf5.txt >> $FILE
+echo "--" >> $FILE
+echo "## PLASMA" >> $FILE
+cat _plasma.txt >> $FILE
+echo "## KDE APPS" >> $FILE
+cat _kde_apps.txt >> $FILE
+echo "## OTHERS" >> $FILE
+cat _others.txt >> $FILE
+
+echo $FILE generated
+
+cat nightly.txt | sort > /tmp/nightly.txt.sorted
+cat $FILE | sort > /tmp/nightly.new.sorted
+
+diff -y --suppress-common-lines --suppress-blank-empty /tmp/nightly.txt.sorted /tmp/nightly.new.sorted
+
+
+if [ "$1" == "update" ]; then
+    echo updating nightly.txt with $FILE
+    cp -av $FILE nightly.txt
+fi
