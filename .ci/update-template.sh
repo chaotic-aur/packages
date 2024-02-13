66a8c3af8432829ff5d2cae629b48a0f99793e97
Nico Jensch <root@dr460nf1r3.org>
2024-02-13 13:31:27 +0100

    feat: add CI-specific syncing of CI stuff

--- .ci/update-template.sh
+++ .ci/update-template.sh
@@ -6,19 +6,27 @@ set -x
 
 CURRENT_REV="$(git rev-parse --abbrev-ref HEAD)"
 
+if [ -v GITLAB_CI ]; then
+    CI_RSYNC_ARGS=("--include" ".gitlab-ci.yml")
+    CI_COMMIT_MSG=("-m" "chore(ci): update CI from template")
+    CI_PUSH_ARGS=("-o" "ci.skip")
+elif [ -v GITHUB_ACTIONS ]; then
+    CI_RSYNC_ARGS=("--include" ".github"
+        "--include" ".github/workflows"
+        "--include" ".github/workflows/chaotic-on-commit.yml"
+        "--include" ".github/workflows/chaotic-on-schedule.yml")
+    CI_COMMIT_MSG=("-m" "chore(ci): update CI from template [skip ci]")
+fi
+
 git clone --depth=1 "$TEMPLATE_REPO" "$TMPDIR/template" -b "main"
 
 trap "git reset --hard $CURRENT_REV" ERR
 
-rsync -a --delete --include ".gitlab-ci.yml" \
-    --exclude ".ci/config" \
+# Generic template files
+rsync -a --delete --exclude ".ci/config" \
     --include ".ci/***" \
     --include ".editorconfig" \
     --include ".envrc" \
-    --include ".github" \
-    --include ".github/workflows" \
-    --include ".github/workflows/chaotic-on-commit.yml" \
-    --include ".github/workflows/chaotic-on-schedule.yml" \
     --include ".markdownlint.yaml" \
     --include ".prettierignore" \
     --include ".shellcheckrc" \
@@ -27,18 +35,19 @@ rsync -a --delete --include ".gitlab-ci.yml" \
     --include "flake.lock" \
     --include "flake.nix" \
     --include "shell.nix" \
+    "${CI_RSYNC_ARGS[@]}" \
     --exclude "*" \
     "$TMPDIR/template/" .
 
 if ! git diff --exit-code --quiet; then
     git add .
-    git commit -m "chore(ci): Update CI from template [skip ci]"
+    git commit "${CI_COMMIT_MSG[@]}"
     # Check if the scheduled tag does not exist or scheduled does not point to HEAD. In which case, the scheduled tag will not be pushed
     if ! [ "$(git tag -l "scheduled")" ] || [ "$(git rev-parse HEAD^)" != "$(git rev-parse scheduled)" ]; then
-        git push --atomic origin HEAD:main
+        git push --atomic origin HEAD:main "${CI_PUSH_ARGS[@]}"
     else
         git tag -f scheduled
-        git push --atomic origin HEAD:main +refs/tags/scheduled
+        git push --atomic origin HEAD:main +refs/tags/scheduled "${CI_PUSH_ARGS[@]}"
     fi
     exit 0
 fi
