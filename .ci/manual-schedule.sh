3e7d482844eb11b3c276892205657e9afd16953f
Chaotic CI <ci@chaotic.cx>
2024-02-15 06:52:29 +0000

    chore(ci): update CI from template

--- .ci/manual-schedule.sh
+++ .ci/manual-schedule.sh
@@ -5,16 +5,17 @@ set -x
 source .ci/util.shlib
 
 # This script is used to manually trigger a build via pipeline UI
-# CI_MANUAL_BUILD is a ":" separated list of package names and needs
+# PACKAGES is a ":" separated list of package names and needs
 # to be supplied via pipeline variables
 
 # Read config file into global variables
 UTIL_READ_CONFIG_FILE
 
-if [ "$CI_MANUAL_BUILD" == "all" ]; then
+if [ "$PACKAGES" == "all" ]; then
     .ci/schedule-packages.sh schedule all
     exit 0
 fi
 
-IFS=':' read -r -a PACKAGES <<<"$CI_MANUAL_BUILD"
-.ci/schedule-packages.sh schedule "${PACKAGES[@]}"
+declare -a FINAL=()
+IFS=':' read -r -a FINAL <<<"$PACKAGES"
+.ci/schedule-packages.sh schedule "${FINAL[@]}"
