e716139396e4eedd298615a13d24a96550bf824f
xiota <xiota@users.noreply.github.com>
2024-06-24 12:49:23 +0000

    feat(opentofu): drop, moved to extra

--- opentofu/tofu.fish
+++
@@ -1,9 +0,0 @@
-
-function __complete_tofu
-    set -lx COMP_LINE (commandline -cp)
-    test -z (commandline -ct)
-    and set COMP_LINE "$COMP_LINE "
-    /usr/bin/tofu
-end
-complete -f -c tofu -a "(__complete_tofu)"
-
