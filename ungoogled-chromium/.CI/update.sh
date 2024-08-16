3cf9c1d967d8b2ac1ee870466852fb5289c686a6
xiota <xiota@users.noreply.github.com>
2024-08-16 16:01:01 +0000

    chore(packages): run shfmt on interferes

--- ungoogled-chromium/.CI/update.sh
+++ ungoogled-chromium/.CI/update.sh
@@ -3,11 +3,11 @@
 UPSTREAM_REPO="https://github.com/ungoogled-software/ungoogled-chromium-archlinux.git"
 TMPDIR=$(mktemp -d)
 
-git clone "$UPSTREAM_REPO" "$TMPDIR" &> /dev/null || echo "Failed to clone upstream repository"
+git clone "$UPSTREAM_REPO" "$TMPDIR" &>/dev/null || echo "Failed to clone upstream repository"
 
 # Run this before comparing to avoid unnecessary diffs
 shfmt -w "$TMPDIR/PKGBUILD"
 
 if [[ $(diff -ruN ungoogled-chromium/PKGBUILD "$TMPDIR/PKGBUILD") != "" ]]; then
-    rsync --exclude  "aur" "$TMPDIR"/* ungoogled-chromium || echo "Failed to copy files"
+  rsync --exclude "aur" "$TMPDIR"/* ungoogled-chromium || echo "Failed to copy files"
 fi
