8c2eb62745226e30762c6ce16a8382bd84437b24
Chaotic CI <ci@chaotic.cx>
2024-10-09 07:31:16 +0000

    chore(packages): update packages
    changed: penguins-eggs, google-cloud-cli, jackett, warpgui-bin

--- warpgui-bin/warpgui.sh
+++ warpgui-bin/warpgui.sh
@@ -4,5 +4,5 @@ _APPDIR="/usr/lib/@appname@"
 _RUNNAME="${_APPDIR}/@runname@"
 export PATH="${_APPDIR}:${PATH}"
 export LD_LIBRARY_PATH="${_APPDIR}/lib/x86_64-linux:${LD_LIBRARY_PATH}"
-cd "${_APPDIR}"
+cd "${_APPDIR}" || { echo "Failed to change directory to ${_APPDIR}"; exit 1; }
 exec "${_RUNNAME}" "$@" || exit $?
