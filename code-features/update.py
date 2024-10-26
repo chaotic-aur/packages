5cb198263f109c6beb6427962746e4efd2159d25
Chaotic CI <ci@chaotic.cx>
2024-10-26 09:31:37 +0000

    chore(packages): update packages
    changed: code-marketplace, code-features

--- code-features/update.py
+++ code-features/update.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-# This script can update the content of ./patch.json to the latest version
-# Usage: ./update.py /path/to/extracted/produce.json
-# Where /path/to/extracted/produce.json is extracted from the latest version of official vscode release
+# This script will update the ./patch.json to match the official release
+# Usage: ./update.py <version-number>
+# Where <version-number> is the version of the official release
 
-import sys
 import json
+import os
+import shutil
+import subprocess
+import sys
 
 key_list = [
     "nameShort",
@@ -34,21 +37,40 @@ key_list = [
     "configurationSync.store",
     "editSessions.store",
     "tunnelApplicationName",
-    "tunnelApplicationConfig"
+    "tunnelApplicationConfig",
 ]
 
-product_path = sys.argv[1]
-patch_path = "patch.json"
 
-with open(product_path, "r") as product_file:
-    product_data = json.load(product_file)
+def fetch_product_json(version: str):
+    """Download official release and extract it, then copy product.json to project root"""
+    url = f"https://update.code.visualstudio.com/{version}/linux-x64/stable"
+    download_cmd = ["curl", "-fSL", "-o", "code.tgz", url]
+    subprocess.run(download_cmd)
+    extract_cmd = ["tar", "xvf", "code.tgz"]
+    subprocess.run(extract_cmd)
+    shutil.copy(src="./VSCode-linux-x64/resources/app/product.json", dst=".")
+    shutil.rmtree("./VSCode-linux-x64")
+    os.remove("code.tgz")
 
-patch_data = {}
 
-for key in key_list:
-    patch_data[key] = product_data[key]
+def update_package():
+    """Update the package"""
+    with open("./product.json", "r") as product_file:
+        product_data = json.load(product_file)
 
-patch_data["enableTelemetry"] = False
+    patch_data = {}
 
-with open(patch_path, "w") as patch_file:
-    json.dump(patch_data, patch_file, indent='\t')
+    for key in key_list:
+        patch_data[key] = product_data[key]
+
+    patch_data["enableTelemetry"] = False
+
+    with open("./patch.json", "w") as patch_file:
+        json.dump(patch_data, patch_file, indent="\t")
+
+    subprocess.run(["updpkgsums", "./PKGBUILD"])
+
+
+version = sys.argv[1]
+fetch_product_json(version)
+update_package()
