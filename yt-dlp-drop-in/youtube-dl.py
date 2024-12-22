c1b2eeb7cae26d67a16ff4e0e4d1afa32f15b0fe
xiota <xiota@users.noreply.github.com>
2024-12-22 21:55:00 +0000

    feat(yt-dlp-drop-in): update

--- yt-dlp-drop-in/youtube-dl.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-import yt_dlp
-
-if __name__ == '__main__':
-    yt_dlp.main(['--compat-options', 'youtube-dl'] + sys.argv[1:])
--- yt-dlp-drop-in/youtube-dl.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-YTDLP="$(which yt-dlp)"
-exec "${YTDLP}" --compat-options youtube-dl "$@"
