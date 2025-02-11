03534f97ed8ca3478b184573eaa8eb3e1b8d2f09
xiota <xiota@users.noreply.github.com>
2025-02-11 07:23:11 +0000

    feat(linux-cachyos-lts): switch to updater script

---
+++ linux-cachyos-lts/.CI/update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+UPSTREAM_REPO="https://github.com/CachyOS/linux-cachyos.git"
+TMPDIR=$(mktemp -d)
+
+git clone "$UPSTREAM_REPO" "$TMPDIR" &>/dev/null || echo "Failed to clone upstream repository"
+
+_PKG="linux-cachyos-lts"
+
+# Run this before comparing to avoid unnecessary diffs
+shfmt -w "$TMPDIR/$_PKG/PKGBUILD"
+
+if [[ $(diff -ruN ungoogled-chromium/PKGBUILD "$TMPDIR/PKGBUILD") != "" ]]; then
+  rsync -a --exclude "aur" "$TMPDIR/$_PKG/" "$_PKG" || echo "Failed to copy files"
+fi
