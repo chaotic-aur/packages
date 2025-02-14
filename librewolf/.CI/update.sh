bdaaa982e3e250dc0f9bcb40863ec0ead3b7fe1c
xiota <xiota@users.noreply.github.com>
2025-02-14 07:39:12 +0000

    feat(librewolf): update

---
+++ librewolf/.CI/update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+_PKG="librewolf"
+UPSTREAM_REPO="https://codeberg.org/librewolf/source"
+
+_OLD_VERSION=$(LANG=C LC_ALL=C pacman -Si chaotic-aur/$_PKG | grep -Pom1 '^Version\s+:\s+\K\S+')
+
+_RESPONSE=$(curl -Ssf "${UPSTREAM_REPO}/releases")
+_TAG=$(
+  echo "$_RESPONSE" |
+    grep -Eom1 '/tag/\S+"' |
+    sed -E 's&^.*/([0-9\.]+-[0-9]+)"$&\1&'
+)
+
+if [ $(vercmp "$_TAG" "$_OLD_VERSION") -gt 0 ]; then
+  _RESPONSE=$(curl -Ssf "https://gitlab.com/api/v4/projects/32320088/packages/generic/librewolf-source/${_TAG}/librewolf-${_TAG}.source.tar.gz.sha256sum")
+
+  _SUM=$(
+    echo "$_RESPONSE" |
+      cut -d' ' -f1
+  )
+
+  sed -E \
+    -e 's&^(: \$\{_pkgrel):=.*&\1:='"${_TAG##*-}\}&" \
+    -e 's&^(: \$\{_cksum):=.*&\1:='"${_SUM##*-}\}&" \
+    -e 's&^(pkgver)=.*$&\1='"${_TAG%%-*}&" \
+    -e 's&^(pkgrel)=.*$&\1='"${_TAG##*-}&" \
+    -i "$_PKG/PKGBUILD"
+fi
