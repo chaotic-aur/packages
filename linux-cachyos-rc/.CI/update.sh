4c554b54bbf8c0e7fd55536d6cece584277f1585
xiota <xiota@users.noreply.github.com>
2025-07-19 17:36:17 +0000

    feat(linux-cachyos-rc)

---
+++ linux-cachyos-rc/.CI/update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+: ${PACKAGE:=linux-cachyos-rc}
+
+UPSTREAM_REPO="https://github.com/CachyOS/linux-cachyos.git"
+TMPDIR=$(mktemp -d)
+
+git clone "$UPSTREAM_REPO" "$TMPDIR" &>/dev/null || echo "Failed to clone upstream repository"
+
+# Run this before comparing to avoid unnecessary diffs
+shfmt -w "$TMPDIR/$PACKAGE/PKGBUILD"
+
+if [[ $(diff -ruN "PKGBUILD" "$TMPDIR/$PACKAGE/PKGBUILD") != "" ]]; then
+  rsync -a --exclude "aur" "$TMPDIR/$PACKAGE/" "." || echo "Failed to copy files"
+fi
