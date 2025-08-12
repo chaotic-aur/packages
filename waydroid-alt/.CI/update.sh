596211a686782188d36bf90c7cc7689b06cadf76
xiota <xiota@users.noreply.github.com>
2025-08-12 15:58:51 +0000

    feat(waydroid-alt): update update.sh

--- waydroid-alt/.CI/update.sh
+++ waydroid-alt/.CI/update.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
-eval "$(grep -Eo '^(url|pkgver|pkgrel)=\S+' PKGBUILD)"
-_OLD_VERSION="$pkgver"
+_update_package() {
+  local _OLD_VERSION _NEW_VERSION _RESPONSE
 
-_RESPONSE=$(curl -Ssf "$url/releases.atom")
-_TAG=$(
-  echo "${_RESPONSE:?}" |
-    grep -Pom1 '(?<=/releases/tag/)[0-9\.-]+(?=")'
-)
+  eval "$(grep -Eo '^(url|pkgver|pkgrel)=\S+' PKGBUILD)"
+  _OLD_VERSION="$pkgver"
 
-if [ "${_TAG:?}" != "${_OLD_VERSION:?}" ]; then
-  sed -E \
-    -e 's&^(pkgver)=.*$&\1='"${_TAG}&" \
-    -e 's&^(pkgrel)=.*$&\1=1&' \
-    -i "PKGBUILD"
-fi
+  _RESPONSE=$(curl -Ssf "$url/releases.atom")
+  _NEW_VERSION=$(
+    echo "${_RESPONSE:?}" |
+      grep -Pom1 '(?<=/releases/tag/)[0-9\.-]+(?=")'
+  )
+
+  if [[ "${_NEW_VERSION:?}" > "${_OLD_VERSION:?}" ]]; then
+    sed -E \
+      -e 's&^(pkgver)=.*$&\1='"${_NEW_VERSION}&" \
+      -e 's&^(pkgrel)=.*$&\1=1&' \
+      -i "PKGBUILD"
+
+    sed -E \
+      -e 's&^(pkgver)=.*$&\1 = '"${_NEW_VERSION}&" \
+      -e 's&^(pkgrel)=.*$&\1 = 1&' \
+      -i ".SRCINFO"
+  fi
+}
+_update_package
+unset -f _update_package
