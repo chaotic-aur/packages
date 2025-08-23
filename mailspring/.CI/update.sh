7098bfec9f26ecccd0962358a91be394f087c914
xiota <xiota@users.noreply.github.com>
2025-08-23 16:44:28 +0000

    feat(mailspring)

---
+++ mailspring/.CI/update.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+_update_package() {
+  local _OLD_VERSION _NEW_VERSION _RESPONSE
+
+  eval "$(grep -Eo '^(url|pkgver|pkgrel)=\S+' PKGBUILD)"
+  _OLD_VERSION="$pkgver"
+
+  _RESPONSE=$(curl -Ssf "$url/tags.atom")
+  _NEW_VERSION=$(
+    echo "${_RESPONSE:?}" |
+      grep -Pom1 '(?<=/releases/tag/)[0-9\.-]+(?=")'
+  )
+
+  if [[ "${_NEW_VERSION:?}" > "${_OLD_VERSION:?}" ]]; then
+    sed -E \
+      -e 's&^(pkgver)=.*$&\1='"${_NEW_VERSION}&" \
+      -e 's&^(pkgrel)=.*$&\1=1&' \
+      -i "PKGBUILD"
+
+    sed -E \
+      -e 's&^(pkgver)=.*$&\1 = '"${_NEW_VERSION}&" \
+      -e 's&^(pkgrel)=.*$&\1 = 1&' \
+      -i ".SRCINFO"
+  fi
+}
+_update_package
+unset -f _update_package
