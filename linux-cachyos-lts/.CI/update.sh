f3e4cc996fb78ea409dc61480d8e7cba1e06d903
xiota <xiota@users.noreply.github.com>
2025-10-25 18:02:10 +0000

    feat(linux-cachyos-lts): update interfere

--- linux-cachyos-lts/.CI/update.sh
+++ linux-cachyos-lts/.CI/update.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env bash
 
-_check_options() {
-  touch default-options.txt # make sure exists
-  local _options_hash_old="$(sha256sum default-options.txt)"
-  mv default-options.txt default-options.old.txt
-
-  grep -E ':\s*"?\s*\$' PKGBUILD > default-options.txt
-  local _options_hash_new="$(sha256sum default-options.txt)"
-
-  if [ "${_options_hash_old:-old}" != "${_options_hash_new:-new}" ]; then
-    echo
-    echo ">>> >>> >>> default options changed <<< <<< <<<"
-    echo
-    diff -U 0 --label old/default-options.txt --label new/default-options.txt default-options.old.txt default-options.txt
-    echo
-    echo ">>> >>> >>> default options changed <<< <<< <<<"
-    echo
-  fi
-  rm -f default-options.old.txt
-}
-
 _update_package() {
   local PACKAGE url TMPDIR
 
@@ -29,14 +9,14 @@ _update_package() {
   git clone "$url.git" "$TMPDIR" &> /dev/null || echo "Failed to clone upstream repository"
 
   # Run this before comparing to avoid unnecessary diffs
-  shfmt -w "$TMPDIR/$PACKAGE/PKGBUILD"
+  shfmt -i 2 -ln=bash -bn -ci -sr -w "$TMPDIR/$PACKAGE/PKGBUILD"
 
   if [[ $(diff -ruN "PKGBUILD" "$TMPDIR/$PACKAGE/PKGBUILD") != "" ]]; then
-    rsync -a --exclude "aur" "$TMPDIR/$PACKAGE/" "." || echo "Failed to copy files"
-  fi
+    rsync -a --delete --exclude "aur" --exclude ".CI" "$TMPDIR/$PACKAGE/" "." || echo "Failed to copy files"
 
-  _check_options
+    # track changes
+    grep -E ':\s*"?\s*\$' PKGBUILD > default-options.txt
+  fi
 }
 _update_package linux-cachyos-lts
 unset -f _update_package
-unset -f _check_options
