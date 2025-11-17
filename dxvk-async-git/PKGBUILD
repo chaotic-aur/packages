b12788268f09fc59c707d7056b03d78607edb882
Chaotic CI <ci@chaotic.cx>
2025-11-17 18:40:59 +0000

    chore(update): update packages (4)
    changed: docker-rootless-extras, topgrade, mullvad-vpn-beta, dxvk-async-git

--- dxvk-async-git/PKGBUILD
+++ dxvk-async-git/PKGBUILD
@@ -5,14 +5,15 @@
 
 _pkgname="dxvk"
 pkgname="$_pkgname-async-git"
-pkgver=2.6.r13.g90ee5fe
-pkgrel=2
+pkgver=2.7.1.r231.g3924944
+pkgrel=1
 pkgdesc="A Vulkan-based compatibility layer for Direct3D 9/10/11 - Windows DLL version"
 url="https://gitlab.com/Ph42oN/dxvk-gplasync/"
 license=('Zlib')
 arch=('x86_64')
 
 depends=(
+  'bash'
   'vulkan-icd-loader'
 )
 makedepends=(
@@ -29,25 +30,19 @@ optdepends=(
 provides=("dxvk=$pkgver")
 conflicts=('dxvk')
 
-options=('!strip' '!buildflags' 'staticlibs')
-backup=('etc/environment.d/dxvk-async-env.conf')
+options=('!strip' '!buildflags')
 
 _pkgsrc="$_pkgname"
+_dxvk_gplasync="dxvk-gplasync-master-$(\date +%Y%m%d.%H)"
 source=(
   "$_pkgsrc"::"git+https://github.com/doitsujin/dxvk.git"
-  "git+https://github.com/Joshua-Ashton/mingw-directx-headers.git"
-  "git+https://github.com/KhronosGroup/Vulkan-Headers.git"
-  "git+https://github.com/KhronosGroup/SPIRV-Headers.git"
-  "git+https://gitlab.com/Ph42oN/dxvk-gplasync.git"
-  "setup_dxvk.sh"
+  "$_dxvk_gplasync.patch"::"https://gitlab.com/Ph42oN/dxvk-gplasync/-/raw/main/patches/dxvk-gplasync-master.patch"
+  'setup_dxvk.sh' # from aur/dxvk-bin
 )
 sha256sums=(
   'SKIP'
   'SKIP'
-  'SKIP'
-  'SKIP'
-  'SKIP'
-  'c5ac42de128630e6d8a9e46a7729630edbfb7626eb293ec5c74c8210c932f0e7'
+  '1feb5c6cfd0f97402893f4bef3496b07ee76438c3b50a0d4b58aeb5c3c27bc82'
 )
 
 pkgver() {
@@ -57,14 +52,9 @@ pkgver() {
 
 prepare() {
   cd "$_pkgsrc"
+  git submodule update --init --recursive --depth=1
 
-  git submodule init include/{native/directx,vulkan,spirv}
-  git config submodule.include/native/directx.url "$srcdir/mingw-directx-headers"
-  git config submodule.include/vulkan.url "$srcdir/Vulkan-Headers"
-  git config submodule.include/spirv.url "$srcdir/SPIRV-Headers"
-  git -c protocol.file.allow=always submodule update include/{native/directx,vulkan,spirv}
-
-  patch -Np1 -F100 -i "$srcdir/dxvk-gplasync/patches/dxvk-gplasync-master.patch"
+  patch -Np1 -F100 -i "$srcdir/$_dxvk_gplasync.patch"
 }
 
 build() {
@@ -75,32 +65,38 @@ build() {
   )
 
   local _meson_x64=(
+    "${_meson_args[@]}"
     --cross-file "$_pkgsrc"/build-win64.txt
     --prefix "/usr/share/dxvk/x64"
     "$_pkgsrc" "build/x64"
   )
 
-  meson setup "${_meson_args[@]}" "${_meson_x64[@]}"
-  meson compile -C build/x64
-
   local _meson_x32=(
+    "${_meson_args[@]}"
     --cross-file "$_pkgsrc"/build-win32.txt
     --prefix "/usr/share/dxvk/x32"
     "$_pkgsrc" "build/x32"
   )
 
-  meson setup "${_meson_args[@]}" "${_meson_x32[@]}"
+  echo "### Building dxvk for win64..."
+  arch-meson "${_meson_x64[@]}"
+  meson compile -C build/x64
+
+  echo "### Building dxvk for win32..."
+  arch-meson "${_meson_x32[@]}"
   meson compile -C build/x32
 }
 
 package() {
-  meson install -C build/x64 --destdir "$pkgdir"
   meson install -C build/x32 --destdir "$pkgdir"
+  meson install -C build/x64 --destdir "$pkgdir"
+
+  find "$pkgdir" -name "*.dll.a" -delete
 
   install -Dm755 "setup_dxvk.sh" "$pkgdir/usr/share/dxvk/setup_dxvk.sh"
 
-  install -dm755 "$pkgdir/usr/bin"
-  ln -srf "$pkgdir/usr/share/dxvk/setup_dxvk.sh" "$pkgdir/usr/bin/setup_dxvk"
+  mkdir -pm755 "$pkgdir/usr/bin"
+  ln -sf "/usr/share/dxvk/setup_dxvk.sh" "$pkgdir/usr/bin/setup_dxvk"
 
   install -Dm644 "$_pkgsrc/LICENSE" -t "$pkgdir/usr/share/licenses/$pkgname/"
 }
--- dxvk-async-git/setup_dxvk.sh
+++ dxvk-async-git/setup_dxvk.sh
@@ -38,6 +38,23 @@ while (($# > 0)); do
   shift
 done
 
+wineserver="wineserver"
+# $PATH is the way for user to control where wine is located (including custom Wine versions).
+# Pure 64-bit Wine (non Wow64) requires skipping 32-bit steps.
+# In such case, wine64 and winebooot will be present, but wine binary will be missing,
+# however it can be present in other PATHs, so it shouldn't be used, to avoid versions mixing.
+wine_path=$(dirname "$(which $wineserver)")
+echo "Using Wine installation in $(dirname $wine_path)"
+wine="$wine_path/wine"
+wine64="$wine_path/wine64"
+
+if [ -z "$WINEPREFIX" ]; then
+  WINEPREFIX="$HOME/.wine"
+fi
+
+# Wait for any existing wine processes in the current prefix to exit
+$wineserver -w
+
 # check wine prefix before invoking wine, so that we
 # don't accidentally create one if the user screws up
 if [ -n "$WINEPREFIX" ] && ! [ -f "$WINEPREFIX/system.reg" ]; then
@@ -51,48 +68,68 @@ export WINEDEBUG=-all
 # wine gecko and mono
 export WINEDLLOVERRIDES="mscoree,mshtml="
 
-wine="wine"
-wine64="wine64"
-wineboot="wineboot"
+# Thank you winetricks!
+get_file_arch() {
+  _W_file="$1"
+  # Assume ELF binaries for everything else
+  _W_ob_output="$(od -An -t x1 -j 0x12 -N 1 "${_W_file}" | tr -d "[:space:]")"
+  case "${_W_ob_output}" in
+    "3e") _W_file_arch="x86_64" ;;
+    "03" | "06") _W_file_arch="i386" ;;
+    *) _W_file_arch="" ;;
+  esac
 
-# $PATH is the way for user to control where wine is located (including custom Wine versions).
-# Pure 64-bit Wine (non Wow64) requries skipping 32-bit steps.
-# In such case, wine64 and winebooot will be present, but wine binary will be missing,
-# however it can be present in other PATHs, so it shouldn't be used, to avoid versions mixing.
-wine_path=$(dirname "$(which $wineboot)")
-wow64=true
-if ! [ -f "$wine_path/$wine" ]; then
-  wine=$wine64
-  wow64=false
+  echo "${_W_file_arch}"
+}
+
+if [ -z "$WINEARCH" ]; then
+  wine_arch=$(get_file_arch "$wine")
+  wine64_arch=$(get_file_arch "$wine64")
+  if [ -f "$WINEPREFIX"/system.reg ]; then
+    arch="$(grep "^#arch=win" "$WINEPREFIX"/system.reg)"
+    arch="${arch##*=}"
+  elif [ "$wine_arch" == "x86_64" ] || [ "$wine64_arch" == "x86_64" ]; then
+    arch=win64
+  elif [ "$wine_arch" == "i386" ] && [ "$wine64_arch" == "" ]; then
+    arch=win32
+  fi
+else
+  arch="$WINEARCH"
+fi
+
+if ! [ -f "$wine64" ]; then
+  wine64="$wine"
+fi
+
+if [ "$arch" == "win32" ]; then
+  wine64="$wine"
 fi
 
 # resolve 32-bit and 64-bit system32 path
-winever=$($wine --version | grep wine)
-if [ -z "$winever" ]; then
-  echo "$wine:"' Not a wine executable. Check your $wine.' >&2
+wine_ver="$($wineserver --version 2>&1 > /dev/null | grep Wine)"
+if [ -z "$wine_ver" ]; then
+  echo "$wineserver:"' Not a wine executable. Check your wine install.' >&2
   exit 1
 fi
 
+wineboot="$wine64 wineboot.exe"
 # ensure wine placeholder dlls are recreated
 # if they are missing
 $wineboot -u
 
-win64_sys_path=$($wine64 winepath -u 'C:\windows\system32' 2> /dev/null)
-win64_sys_path="${win64_sys_path/$'\r'/}"
-if $wow64; then
-  win32_sys_path=$($wine winepath -u 'C:\windows\system32' 2> /dev/null)
-  win32_sys_path="${win32_sys_path/$'\r'/}"
-fi
+system32_path=$($wine64 winepath.exe -u 'C:\windows\system32' 2> /dev/null)
+system32_path="${system32_path/$'\r'/}"
+syswow64_path=$($wine64 winepath.exe -u 'C:\windows\syswow64' 2> /dev/null)
+syswow64_path="${syswow64_path/$'\r'/}"
 
-if [ -z "$win32_sys_path" ] && [ -z "$win64_sys_path" ]; then
+if [ -z "$syswow64_path" ] && [ -z "$system32_path" ]; then
   echo 'Failed to resolve C:\windows\system32.' >&2
   exit 1
 fi
 
 # create native dll override
 overrideDll() {
-  $wine reg add 'HKEY_CURRENT_USER\Software\Wine\DllOverrides' /v $1 /d native /f > /dev/null 2>&1
-  if [ $? -ne 0 ]; then
+  if ! $wine64 reg add 'HKEY_CURRENT_USER\Software\Wine\DllOverrides' /v "$1" /d native /f > /dev/null 2>&1; then
     echo -e "Failed to add override for $1"
     exit 1
   fi
@@ -100,8 +137,7 @@ overrideDll() {
 
 # remove dll override
 restoreDll() {
-  $wine reg delete 'HKEY_CURRENT_USER\Software\Wine\DllOverrides' /v $1 /f > /dev/null 2>&1
-  if [ $? -ne 0 ]; then
+  if ! $wine64 reg delete 'HKEY_CURRENT_USER\Software\Wine\DllOverrides' /v "$1" /f > /dev/null 2>&1; then
     echo "Failed to remove override for $1"
   fi
 }
@@ -110,10 +146,6 @@ restoreDll() {
 installFile() {
   dstfile="${1}/${3}.dll"
   srcfile="${basedir}/${2}/${3}.dll"
-
-  if [ -f "${srcfile}.so" ]; then
-    srcfile="${srcfile}.so"
-  fi
 
   if ! [ -f "${srcfile}" ]; then
     echo "${srcfile}: File not found. Skipping." >&2
@@ -127,11 +159,10 @@ installFile() {
       else
         rm -v "${dstfile}"
       fi
-      $file_cmd "${srcfile}" "${dstfile}"
     else
-      echo "${dstfile}: File not found in wine prefix" >&2
-      return 1
+      touch "${dstfile}.old_none"
     fi
+    $file_cmd "${srcfile}" "${dstfile}"
   fi
   return 0
 }
@@ -140,10 +171,6 @@ installFile() {
 uninstallFile() {
   dstfile="${1}/${3}.dll"
   srcfile="${basedir}/${2}/${3}.dll"
-
-  if [ -f "${srcfile}.so" ]; then
-    srcfile="${srcfile}.so"
-  fi
 
   if ! [ -f "${srcfile}" ]; then
     echo "${srcfile}: File not found. Skipping." >&2
@@ -159,38 +186,50 @@ uninstallFile() {
     rm -v "${dstfile}"
     mv -v "${dstfile}.old" "${dstfile}"
     return 0
+  elif [ -f "${dstfile}.old_none" ]; then
+    rm -v "${dstfile}.old_none"
+    rm -v "${dstfile}"
+    return 0
   else
     return 1
   fi
 }
 
 install() {
-  installFile "$win64_sys_path" "$dxvk_lib64" "$1"
-  inst64_ret="$?"
-
-  inst32_ret=-1
-  if $wow64; then
-    installFile "$win32_sys_path" "$dxvk_lib32" "$1"
-    inst32_ret="$?"
+  if [ "$arch" == "win32" ]; then
+    lib="$dxvk_lib32"
+    dll="${1}"
+  else
+    lib="$dxvk_lib64"
+    dll="${1}"
+  fi
+  if installFile "$system32_path" "$lib" "$dll"; then
+    overrideDll "$dll"
   fi
 
-  if ((($inst32_ret == 0) || ($inst64_ret == 0))); then
-    overrideDll "$1"
+  if [ -d "$syswow64_path" ]; then
+    if installFile "$syswow64_path" "$dxvk_lib32" "$1"; then
+      overrideDll "$1"
+    fi
   fi
 }
 
 uninstall() {
-  uninstallFile "$win64_sys_path" "$dxvk_lib64" "$1"
-  uninst64_ret="$?"
-
-  uninst32_ret=-1
-  if $wow64; then
-    uninstallFile "$win32_sys_path" "$dxvk_lib32" "$1"
-    uninst32_ret="$?"
+  if [ "$arch" == "win32" ]; then
+    lib="$dxvk_lib32"
+    dll="${1}"
+  else
+    lib="$dxvk_lib64"
+    dll="${1}"
+  fi
+  if uninstallFile "$system32_path" "$lib" "$dll"; then
+    restoreDll "$dll"
   fi
 
-  if ((($uninst32_ret == 0) || ($uninst64_ret == 0))); then
-    restoreDll "$1"
+  if [ -d "$syswow64_path" ]; then
+    if uninstallFile "$syswow64_path" "$dxvk_lib32" "$1"; then
+      restoreDll "$1"
+    fi
   fi
 }
 
