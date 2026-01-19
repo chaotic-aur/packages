1a4d57455700aae3fc1e3dfe5aa17530a87ab9f1
xiota <xiota@users.noreply.github.com>
2026-01-19 22:46:41 +0000

    feat(autofirma)

---
+++ autofirma/autofirma.js
@@ -0,0 +1,3 @@
+pref("network.protocol-handler.app.afirma","/usr/bin/autofirma");
+pref("network.protocol-handler.warn-external.afirma",false);
+pref("network.protocol-handler.external.afirma",true);
