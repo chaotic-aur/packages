607a8707e9d1d708a2997dd7587be5e4dad41fcc
Chaotic CI <ci@chaotic.cx>
2026-01-26 03:38:11 +0000

    chore(vscodium-electron): PKGBUILD modified

--- vscodium-electron/vscodium.sh
+++ vscodium-electron/vscodium.sh
@@ -1,7 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "${XDG_SESSION_TYPE}" = "wayland" ]; then
-   exec @ELECTRON@ /usr/lib/vscodium/out/cli.js --enable-features=UseOzonePlatform --ozone-platform=wayland /usr/lib/vscodium/vscodium.js "$@"
-else
-   ELECTRON_RUN_AS_NODE=1 exec @ELECTRON@ /usr/lib/vscodium/out/cli.js /usr/lib/vscodium/vscodium.js "$@"
+set -euo pipefail
+
+flags_file="${XDG_CONFIG_HOME:-$HOME/.config}/codium-flags.conf"
+
+if [[ -f "${flags_file}" ]]; then
+    mapfile -t < "${flags_file}" CODEMAPFILE
 fi
+
+codeflags=()
+for line in "${CODEMAPFILE[@]}"; do
+    if [[ ! "${line}" =~ ^[[:space:]]*#.* ]] && [[ -n "${line}" ]]; then
+        codeflags+=("${line}")
+    fi
+done
+
+# don't edit the electron binary name here! simply change the variable in the PKGBUILD and we will sed it into the correct one :)
+name=@ELECTRON@
+flags_file="${XDG_CONFIG_HOME:-$HOME/.config}/@ELECTRON@-flags.conf"
+fallback_file="${XDG_CONFIG_HOME:-$HOME/.config}/electron-flags.conf"
+
+lines=()
+if [[ -f "${flags_file}" ]]; then
+    mapfile -t lines < "${flags_file}"
+elif [[ -f "${fallback_file}" ]]; then
+    mapfile -t lines < "${fallback_file}"
+fi
+
+electronflags=()
+for line in "${lines[@]}"; do
+    if [[ ! "${line}" =~ ^[[:space:]]*#.* ]] && [[ -n "${line}" ]]; then
+        electronflags+=("${line}")
+    fi
+done
+
+ELECTRON_RUN_AS_NODE=1 exec /usr/lib/@ELECTRON@/electron /usr/lib/vscodium/out/cli.js "${electronflags[@]}" /usr/lib/vscodium/vscodium.js "${codeflags[@]}" "$@"
