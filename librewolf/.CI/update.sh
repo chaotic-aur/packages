cebb3d481afb9af12215dc238b69de6f0faf8ed7
xiota <xiota@users.noreply.github.com>
2026-02-17 17:53:13 +0000

    chore(update): librewolf

--- librewolf/.CI/update.sh
+++ librewolf/.CI/update.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 _update_package() (
-  eval "$(grep -Eo '^(url|pkgver|pkgrel|.*cksum:?)=\S+' PKGBUILD)"
+  eval "$(grep -Eo '^(url|pkgver|pkgrel)=\S+' PKGBUILD)"
   eval "$(grep -E '^: \${_pkgrel' PKGBUILD)"
   _OLD_VERSION="$pkgver-$pkgrel"
+  _OLD_SUM="$(grep -m1 'update-cksum' PKGBUILD | grep -Pom1 '[0-9a-f]{64}')"
 
   _RESPONSE=$(curl -Ssf "$url/releases.atom")
   _NEW_VERSION=$(
@@ -19,14 +20,14 @@ _update_package() (
     )
 
     sed -E \
+      -e "s@${_OLD_SUM}@${_SUM##*-}@" \
       -e 's&^(: \$\{_pkgrel):=.*&\1:='"${_NEW_VERSION##*-}\}&" \
-      -e 's&^(: \$\{_cksum):=.*&\1:='"${_SUM##*-}\}&" \
       -e 's&^(pkgver)=.*$&\1='"${_NEW_VERSION%%-*}&" \
       -e 's&^(pkgrel)=.*$&\1='"${_NEW_VERSION##*-}&" \
       -i "PKGBUILD"
 
     sed -E \
-      -e "s@${_cksum}@${_SUM##*-}@" \
+      -e "s@${_OLD_SUM}@${_SUM##*-}@" \
       -e 's@^(\s*pkgver\s*=\s*).*$@\1'"${_NEW_VERSION%%-*}@" \
       -e 's@^(\s*pkgrel\s*=\s*).*$@\1'"${_NEW_VERSION##*-}@" \
       -e '/source = /s&'"${_OLD_VERSION%-*}-${_pkgrel:-1}&${_NEW_VERSION}&g" \
