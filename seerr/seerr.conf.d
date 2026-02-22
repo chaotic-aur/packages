e3a7d5af8cf5f6385356c28da6d9f01a7898b41d
xiota <xiota@users.noreply.github.com>
2026-02-22 04:43:53 +0000

    feat(seerr)

---
+++ seerr/seerr.conf.d
@@ -0,0 +1,11 @@
+# Seerr's default port is 5055, if you want to use both, change this.
+# specify on which port to listen
+PORT=5055
+
+# specify on which interface to listen, by default seerr listens on all interfaces
+#HOST=127.0.0.1
+
+# Uncomment if your media server is emby instead of jellyfin.
+#JELLYFIN_TYPE=emby
+
+# For database configuration options see https://docs.seerr.dev/extending-seerr/database-config
