4c7fb0d9ae318726334a4e959c1ceca553c47e63
xiota <xiota@users.noreply.github.com>
2026-02-22 02:26:14 +0000

    feat(waterfox): switch to custom package

---
+++ waterfox/.CI/update.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+_update_package() {
+  local _OLD_VERSION _NEW_VERSION _RESPONSE
+
+  eval "$(grep -Eo '^(url|pkgver|pkgrel)=\S+' PKGBUILD)"
+  _OLD_VERSION="$pkgver"
+
+  _RESPONSE=$(curl -Ssf "$url/releases.atom")
+  _NEW_VERSION=$(
+    grep -Pom1 '(/releases/tag/[^0-9]*)\K[0-9\.-]+(?=")' <<< "${_RESPONSE:?}"
+  )
+
+  if (($(vercmp "${_NEW_VERSION:?}" "${_OLD_VERSION:?}") > 0)); then
+    sed -E \
+      -e 's&^(\s*pkgver\s*=\s*).*$&\1'"${_NEW_VERSION}&" \
+      -e 's&^(\s*pkgrel\s*=\s*).*$&\11&' \
+      -i "PKGBUILD" ".SRCINFO"
+
+    sed -E \
+      -e '/source = /s&'"${_OLD_VERSION}&${_NEW_VERSION}&g" \
+      -i ".SRCINFO"
+  fi
+}
+_update_package
+unset -f _update_package
