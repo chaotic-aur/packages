80c8fdad8276bdc3e5fcfde63bae8891f51e8bdc
Chaotic CI <ci@chaotic.cx>
2026-02-28 12:35:13 +0000

    chore(update): dwm

--- dwm/config.h
+++ dwm/config.h
@@ -36,6 +36,7 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
