6909253743fd29e71fb5ede3a45c5ab5cadd8c10
Chaotic CI <ci@chaotic.cx>
2026-03-20 03:37:24 +0000

    chore(update): irpf

--- irpf/irpf.sh
+++ irpf/irpf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export PATH="/usr/lib/jvm/java-11-openjdk/bin:/usr/lib/jvm/java-11-jdk/bin${PATH:+":${PATH}"}"
+export PATH="/usr/lib/jvm/java-17-openjdk/bin:/usr/lib/jvm/java-17-jdk/bin${PATH:+":${PATH}"}"
 exec java -Xmx512m -jar /usr/share/java/irpf/irpf.jar "$@"
