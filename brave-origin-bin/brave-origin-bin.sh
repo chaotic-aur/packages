9e4cf64957305c953a9ec3f7595cdbba748abef6
Chaotic CI <ci@chaotic.cx>
2026-06-13 10:33:02 +0000

    feat(add): brave-origin-bin

---
+++ brave-origin-bin/brave-origin-bin.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"${HOME}/.config"}"
+
+CONF_FILE="${XDG_CONFIG_HOME}/brave-origin-flags.conf"
+
+if
+	test -f "${CONF_FILE}"
+then
+	mapfile -t CONF_LIST < "${CONF_FILE}"
+fi
+
+for CONF_LINE in "${CONF_LIST[@]}"
+do
+	if ! [[
+		"${CONF_LINE}" =~ ^[[:space:]]*(#|$)
+	]]
+	then
+		FLAG_LIST+=("${CONF_LINE}")
+	fi
+done
+
+export CHROME_VERSION_EXTRA='stable'
+
+exec /opt/brave-origin-bin/brave-origin "${FLAG_LIST[@]}" "${@}"
