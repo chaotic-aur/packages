d2ff1b34652a1d3d2a73f44e1bf9b2254832194f
Chaotic CI <ci@chaotic.cx>
2026-07-03 12:41:21 +0000

    chore(update): shutter-encoder-bin

--- shutter-encoder-bin/shutter-encoder.sh
+++ shutter-encoder-bin/shutter-encoder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -o pipefail
 _APPDIR="/usr/lib/@appname@"
-_RUNNAME="${_APPDIR}/@runname@"
-export PATH="${_APPDIR}/usr/bin:${_APPDIR}/usr/bin/JRE/bin:${_APPDIR}/usr/bin/Library:$PATH}"
-export LD_LIBRARY_PATH="${_APPDIR}/usr/lib/x86_64-linux-gnu:${_APPDIR}/usr/bin/JRE/lib:${LD_LIBRARY_PATH}"
+_RUNNAME="${_APPDIR}/bin/@runname@"
+export PATH="${_APPDIR}/bin:${_APPDIR}/lib/runtime/bin:${_APPDIR}/lib/app/Library:$PATH"
+export LD_LIBRARY_PATH="${_APPDIR}/lib/runtime/lib:${LD_LIBRARY_PATH}"
 cd "${_APPDIR}" || { echo "Failed to change directory to ${_APPDIR}"; exit 1; }
 exec "${_RUNNAME}" "$@" || exit $?
