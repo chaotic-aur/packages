b3d7cda81eb00404a3207b16d81e176169a382b7
Chaotic CI <ci@chaotic.cx>
2026-07-25 15:55:34 +0000

    chore(update): woof-doom

--- woof-doom/install.sh
+++ woof-doom/install.sh
@@ -1,6 +1,6 @@
 post_install() {
     echo "Woof! uses FluidSynth for high-quality MIDI playback."
-    echo "You can install a soundfont package such as 'soundfount-fluid', or add your"
+    echo "You can install a soundfont package such as 'soundfont-fluid', or add your"
     echo "own soundfont directories to soundfont_dir in ~/.local/share/woof/woof.cfg."
     echo "Soundfonts can be selected from the Options -> General menu inside Woof!."
     echo "See also https://wiki.archlinux.org/title/FluidSynth for more information."
