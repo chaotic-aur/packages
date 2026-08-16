4606d7c8653cf2dbc4f8c77f6e0428e90384793d
Chaotic CI <ci@chaotic.cx>
2026-08-16 21:38:08 +0000

    chore(update): ironwail

--- ironwail/launch_ironwail.sh
+++ ironwail/launch_ironwail.sh
@@ -3,4 +3,4 @@
 # Unfortunately creating a symbolic link to ironwail in /usr/bin causes it to not detect the .pak in the destination folder.
 # Thus this helper script has been created to help fix the issue.
 
-/opt/ironwail/ironwail $@
+/opt/ironwail/ironwail "$@"
